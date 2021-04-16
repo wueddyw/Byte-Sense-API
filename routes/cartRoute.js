5c44c5b6264beb34af45bcd2465860c835d2c24b
Eddy Wu <56403971+wueddyw@users.noreply.github.com>
2021-04-16 07:30:00 -0700

    git ignore

--- routes/cartRoute.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const cartController = require("../cart/controller");
-router.post("/", cartController.addItemToCart);
-router.get("/", cartController.getCart);
-router.delete("/empty-cart", cartController.emptyCart);
-router.delete("/remove-item", cartController.removeItemFromCart);
-module.exports = router;
--- routes/fetchData.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const fetchController = require("../DB/fetchController");
-
-router.post("/", fetchController.fetchData);
-
-module.exports = router;
